69075e6c4bf1fa3d3b3875b93b0a494cb3118465
higayasuo <higayasuo@gmail.com>
2022-08-15 05:34:36 +0000

    Refactor escrow_bad

--- src/escrow_bad/escrow_bad_asc1_main.py
+++ src/escrow_bad/escrow_bad_asc1_main.py
@@ -55,7 +55,7 @@ def create_asset(client: AlgodClient, private_key: str, clawback: str) -> int:
         private_key,
         total=1,
         decimals=0,
-        default_frozen=False,
+        default_frozen=True,
         unit_name="ASA",
         asset_name="ASA",
         manager=sender,
@@ -65,72 +65,28 @@ def create_asset(client: AlgodClient, private_key: str, clawback: str) -> int:
     )
 
 
-def opt_in_transfer_asset_fund(
+def buy(
     client: AlgodClient,
     private_key: str,
     asset_sender: bytes | str,
     app_id: int,
     asset_id: int,
 ) -> None:
-    print("opt_in_transfer_asset_fund")
+    print("buy()")
     sender = address_from_private_key(private_key)
     params = client.suggested_params()
 
     txn1 = AssetOptInTxn(sender=sender, sp=params, index=asset_id)
-    app_args = [AppMethods.transfer_asset, asset_id]
     txn2 = ApplicationNoOpTxn(
         sender,
         params,
         app_id,
-        app_args,
+        app_args=[AppMethods.transfer_asset, asset_id],
         accounts=[asset_sender],
     )
     txn3 = PaymentTxn(sender, params, receiver=asset_sender, amt=1000000)
     sign_send_wait_group_transactions(
         client, [txn1, txn2, txn3], [private_key, private_key, private_key]
-    )
-
-
-def return_destroy_asset_delete_app_refund(
-    client: AlgodClient,
-    sender_private_key: str,
-    destroyer_private_key: str,
-    asset_id: int,
-    app_id: int,
-) -> None:
-    print("return_destroy_asset_delete_app_refund")
-    sender = address_from_private_key(sender_private_key)
-    destroyer = address_from_private_key(destroyer_private_key)
-    params = client.suggested_params()
-
-    txn1 = AssetTransferTxn(
-        sender=sender,
-        sp=params,
-        receiver=destroyer,
-        amt=1,
-        index=asset_id,
-        close_assets_to=destroyer,
-    )
-    txn2 = AssetDestroyTxn(
-        sender=destroyer,
-        sp=params,
-        index=asset_id,
-    )
-    txn3 = ApplicationDeleteTxn(
-        sender=destroyer,
-        sp=params,
-        index=app_id,
-    )
-    txn4 = PaymentTxn(destroyer, params, receiver=sender, amt=1000000)
-    sign_send_wait_group_transactions(
-        client,
-        [txn1, txn2, txn3, txn4],
-        [
-            sender_private_key,
-            destroyer_private_key,
-            destroyer_private_key,
-            destroyer_private_key,
-        ],
     )
 
 
@@ -141,20 +97,12 @@ def main():
     asset_id = create_asset(client, test1_private_key, escrow_address)
     fund(client, test1_private_key, receiver=escrow_address, amt=101000)
 
-    opt_in_transfer_asset_fund(
+    buy(
         client,
         test2_private_key,
         asset_sender=test1_address,
         app_id=app_id,
         asset_id=asset_id,
-    )
-
-    return_destroy_asset_delete_app_refund(
-        client,
-        sender_private_key=test2_private_key,
-        destroyer_private_key=test1_private_key,
-        asset_id=asset_id,
-        app_id=app_id,
     )
 
 
